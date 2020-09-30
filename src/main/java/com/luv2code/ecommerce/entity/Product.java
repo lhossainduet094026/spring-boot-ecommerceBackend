6bd2926a150cdbdcd18b200a7e6faa8bfd39edb1
lokman <abc@example.com>
2020-09-30 20:45:36 +0600

    Product entity defined

--- src/main/java/com/luv2code/ecommerce/entity/Product.java
+++ src/main/java/com/luv2code/ecommerce/entity/Product.java
@@ -1,5 +1,25 @@
 package src.main.java.com.luv2code.ecommerce.entity;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name="product")
+@Data//this annotation for lombok project.automatically generate getter/setter
 public class Product {
+private long id;
+private String sku;
+private String name;
+private String description ;
+private BigDecimal unitPrice;
+private String imageUrl;
+private boolean active;
+private int unitsInStock;
+private Date dateCreated;
+private Date lastUpdated;
 
 }
